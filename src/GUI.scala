4b0058f3366902d4160d29ead9e8c7a061974529
hubz00 <hubert.zajac@outlook.com>
2015-01-28 18:01:39 +0100

    ramki

--- src/GUI.scala
+++ src/GUI.scala
@@ -9,28 +9,51 @@ import swing._
 
 object GUI extends SimpleSwingApplication{
 
-
+  val buttonDimension = new Dimension(75,75)
+  val inputLabelDimension = new Dimension(50,50)
+  val resultLabelDimension = new Dimension(25,25)
   val inputGridBackground = Color.LIGHT_GRAY
 
-  val inputButton1 = new Button()
-  val inputButton2 = new Button()
-  val inputButton3 = new Button()
-  val inputButton4 = new Button()
+  val inputButton1 = new Button(){
+    minimumSize = buttonDimension
+    maximumSize = buttonDimension
+    preferredSize = buttonDimension
+  }
+  val inputButton2 = new Button(){
+    minimumSize = buttonDimension
+    maximumSize = buttonDimension
+    preferredSize = buttonDimension
+  }
+  val inputButton3 = new Button(){
+    minimumSize = buttonDimension
+    maximumSize = buttonDimension
+    preferredSize = buttonDimension
+  }
+  val inputButton4 = new Button(){
+    minimumSize = buttonDimension
+    maximumSize = buttonDimension
+    preferredSize = buttonDimension
+  }
 
   var labelsInputTable =  Array.ofDim[Label](12,4)
   for (i <- 0 to 11){
     for (j <- 0 to 3){
         labelsInputTable(i)(j)= new Label(Integer.toString((i+ 1) * (1 + j))){
-          background = inputGridBackground
-          foreground = inputGridBackground
+            minimumSize = inputLabelDimension
+            maximumSize = inputLabelDimension
+            preferredSize = inputLabelDimension
         }
     }
   }
 
-  var labelsResultTable = Array.ofDim[Label](2,2)
+  var labelsResultTable = Array.ofDim[Label](12,4)
     for(i <- 0 to 11){
       for(j <- 0 to 3){
-        labelsResultTable(i)(j) = new Label(Integer.toString((i+1) * (j+1)))
+        labelsResultTable(i)(j) = new Label(Integer.toString((i+1) * (j+1))) {
+          minimumSize = resultLabelDimension
+          maximumSize = resultLabelDimension
+          preferredSize = resultLabelDimension
+        }
       }
     }
 
@@ -44,12 +67,27 @@ object GUI extends SimpleSwingApplication{
     contents += inputButton4
   }
 
-  val inputGrid =  new GridPanel(12,2){
+  val gameGrid =  new GridPanel(12,5){
     for( i <- 0 to 11){
-      for(j <- 0 to 3){
-        contents += labelsInputTable(i)(j)
+      for(j <- 0 to 4){
+        if( j == 4){
+          contents += getResultGrid(i)
+        }
+        else contents += labelsInputTable(i)(j)
       }
     }
+  }
+
+  val ui  = new BorderPanel{
+    add(inputPanel, BorderPanel.Position.West)
+    add(gameGrid, BorderPanel.Position.Center)
+    add(confirmButton, BorderPanel.Position.South)
+  }
+
+  override def top = new MainFrame{
+    title = "MaScalMind"
+    contents = ui
+    visible = true
   }
 
   def getResultGrid (row :Int): GridPanel ={
@@ -60,29 +98,5 @@ object GUI extends SimpleSwingApplication{
     }
     tmpGrid
   }
-  val resultGrid = new GridPanel(2,2){
-    contents += labelsResultTable(0)(0)
-    contents += labelsResultTable(0)(1)
-    contents += labelsResultTable(1)(0)
-    contents += labelsResultTable(1)(1)
-  }
-
-  val gamePanel = new BorderPanel{
-      background = inputGridBackground
-      add(inputGrid, BorderPanel.Position.Center)
-  }
-
-  val ui  = new BorderPanel{
-    add(inputPanel, BorderPanel.Position.West)
-    add(gamePanel, BorderPanel.Position.Center)
-    add(confirmButton, BorderPanel.Position.South)
-  }
-
-  override def top = new MainFrame{
-    title = "MaScalMind"
-    contents = ui
-    visible = true
-  }
-
 
 }
